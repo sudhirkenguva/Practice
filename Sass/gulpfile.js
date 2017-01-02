82ae20a82a3452600dc03c5d4db7fe01515423da
SudhirKumarK <ksudhircse@gmail.com>
2017-01-02 16:17:49 +0530

    Practice

---
+++ Sass/gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var sassdocs
+
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'expand' }))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('../sourcemaps/sass'))
+        .pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('cssmin', ['sass'], function() {
+    return gulp.src('css/*.css')
+        .pipe(cleanCSS({ debug: true }, function(details) {
+            console.log(details.name + '[' + details.stats.originalSize + ' -->' + details.stats.minifiedSize + ' bytes ] ');
+        }))
+        .pipe(rename('test.min.css'))
+        .pipe(gulp.dest('./cssmin'));
+});
+
+
+gulp.task("default", ['cssmin'], function() {
+    gulp.watch('./sass/**/*.scss', ['cssmin']);
+});
---
+++ VS code/temp.js
@@ -0,0 +1,18 @@
+for (let i = 10; i <= 15; i++) {
+    for (let i = 10; i <= 15; i++) {
+        for (let i = 10; i <= 15; i++) {
+            for (let i = 10; i <= 15; i++) {
+                for (let i = 10; i <= 15; i++) {
+                    for (let i = 10; i <= 15; i++) {
+                        for (let i = 10; i <= 15; i++) {
+                            for (let i = 10; i <= 15; i++) {
+                                for (let i = 10; i <= 15; i++) {
+                                    for (let i = 10; i <= 15; i++) {
+                                        for (let i = 10; i <= 15; i++) {
+                                            for (let i = 10; i <= 15; i++) {
+                                                for (let i = 10; i <= 15; i++) {
+                                                    for (let i = 10; i <= 15; i++) {
+                                                        console.log(i);
+                                                    }
+
+                                                    for (let i = 10; i <= 15; i++)
